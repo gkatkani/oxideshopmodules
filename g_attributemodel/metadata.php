9c6a1a5e0d6a11293f3974ff3dc1884a3a71752a
gaurav <gaurav@whitebrains.in>
2018-06-28 20:42:06 +0530

    by gaurav14

--- g_attributemodel/metadata.php
+++ g_attributemodel/metadata.php
@@ -12,25 +12,25 @@ $sMetadataVersion = '2.0';
 
 
 $aModule = array(
-    'id'          => 'oxideshopmodule',
-    'title'       => 'oxideshopmodule extend filter',
+    'id'          => 'g_attributemodel',
+    'title'       => 'Megamenu extend filter',
     'description' =>  array(
         'de'=>'Erweiterter Attributfilter',
         'en'=>'Extended Attribute Filter',
     ),
     'thumbnail'   => 'itratos.png',
-    'version'     => '1.0.0',
+    'version'     => '1.0.0'
     // 'url'         => 'http://gauravkatkani.blogspot.com',
     // 'email'       => 'gkatkani@gmail.com',
     // 'author'      => 'gauravkatkani',
     
-    'files'      => array(
+    // 'files'      => array(
        
-        'gk_megamenu_events' => 'oxideshopmodule/g_attributemodel/events/Gk_megamenu_events.php',
-    ),
+    //     'gk_megamenu_events' => 'oxideshopmodule/g_attributemodel/events/Gk_megamenu_events.php',
+    // ),
     
-    'events'       => array(
-        'onActivate'   => 'gk_megamenu_events::onactivate',
-        'onDeactivate' => 'gk_megamenu_events::ondeactivate'
-    ),
+    // 'events'       => array(
+    //     'onActivate'   => 'gk_megamenu_events::onactivate',
+    //     'onDeactivate' => 'gk_megamenu_events::ondeactivate'
+    // ),
 );
